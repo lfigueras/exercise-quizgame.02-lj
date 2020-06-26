fabb843fb9a511ce020c19a9e4bb2c2e3cbf717d
lfigueras <lovelyfigueras@gmail.com>
2020-06-26 17:53:32 +0800

    70% functionality done.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
    reaches the last question . 
 -Score: this should displays the total score. 
 
-1.Button that will trigger to change the value of the question.
+1.But  on that will trigger to change the value of the question.
 2.Determine the correct answer.
 3.The score will display if the answer is correct (increment) and will not change if its wrong.
 4.After determining the right or wrong question it will proceed to the next question.
@@ -17,6 +17,100 @@ Extra:
 -Question number display changes when the next question proceeds.
 */
 
-document.getElementById('a').addEventListener('click', e =>{
-    
+//Array of questions
+
+let button1 = document.getElementById('a');
+let button2 = document.getElementById('b');
+let button3 = document.getElementById('c');
+let button4 = document.getElementById('d');
+
+let myQuestions = [
+    {
+      question: "How old is LJ?",
+      
+      answers: {
+        a: "27",
+        b: "25",
+        c: "26",
+        d: "24",
+        correctAnswer: "b",
+      },
+    },
+    {
+      question: "What is her favorite game?",
+      answers: {
+        a: "ML",
+        b: "COD",
+        c: "DOTA 2",
+        d: "LOL",
+        correctAnswer: "b",
+      },
+     
+    },
+    {
+      question: "Where does LJ live?",
+      answers: {
+        a: "Pasig city",
+        b: "Quezon city",
+        c: "Rizal",
+        d: "Sta Mesa",
+        correctAnswer: "a",
+      },
+      
+    }
+  ];
+
+
+let lastQuestionIndex = myQuestions.length-1;
+let questionIndex = 0;
+
+
+
+document.getElementById('startbtn').addEventListener('click', e =>{
+  renderQuestion();
+  document.querySelector('.start').classList.add('hide');
+  document.querySelector('.question-container1').classList.remove('hide');
+  document.querySelector('.question-container1').classList.add('show');
+  
 });
+
+document.getElementById('button').addEventListener('click' , e => {
+
+
+   if (questionIndex < 3){
+    
+    renderQuestion();
+    checkAnswer();
+    
+   }else{
+    document.querySelector('.question-container1').classList.add('hide');
+    
+   }
+    
+})
+
+function renderQuestion(){
+
+  let show = document.getElementById('question');
+  let q = myQuestions[questionIndex];
+  
+    show.innerHTML = q.question;
+    questionIndex++;
+    console.log(questionIndex)
+
+    button1.innerHTML = q.answers.a;
+    button2.innerHTML = q.answers.b;
+    button3.innerHTML = q.answers.c;
+    button4.innerHTML = q.answers.d;
+}
+
+function checkAnswer(){
+    
+}
+
+
+
+//if button is pressed ! right move on
+//if button is pressed right answer score ++
+
+
